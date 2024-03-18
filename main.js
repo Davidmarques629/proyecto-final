8e50e94a1fb686769d21807489c927eb869aadc5
Davidmarques629 <115791849+Davidmarques629@users.noreply.github.com>
2024-03-18 13:55:38 +0100

    Cambio

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import './style.css'
+import { maquinasytecnologia } from './secciones';
+import { materialDeportivo } from './secciones';
+import { gastronomia } from './secciones';   
 const tecno=document.querySelector('#tecno');
 const sport=document.querySelector('#sport');
 const foods=document.querySelector('#foods');
+const pintarTecno=(array)=>{
+  tecno.addEventListener  
+}
---
+++ secciones.js
@@ -0,0 +1,38 @@
+export const maquinasytecnologia=[{
+   nombre: "Bitcoin Myner",
+   precio: 69,
+   disponible: true 
+},
+{nombre: "Tablet Android",
+ precio: 20,
+ disponible: tue   
+},
+{nombre: "Portátil HP",
+precio: 100,
+disponible: true;    
+}
+];
+export const materialDeportivo=[{
+nombre: "Balón de fútbol",
+precio: 10,
+disponible: true    
+},
+{nombre: "Balón de baloncesto",
+  precio: 7,
+  disponible: true  
+}
+];
+export const gastronomia=[{
+nombre: "Bandeja de sushi 10 piezas",
+precio: 6,
+disponible: true
+},
+    {nombre: "Hamburguesa de carne con cebolla y pepinillos",
+   precio: 5,
+   disponible: true
+},
+{nombre: "Kevap de ternera con tomate, lechuga y cebolla",
+precio: 15,
+disponible: true
+}
+];
