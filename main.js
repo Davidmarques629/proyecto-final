4b8980a12625a2100b612dde36649733f749f563
Davidmarques629 <david.marquesht@gmail.com>
2024-03-19 21:56:18 +0100

    Nuevas funcionalidades añadidas

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
-      import './style.css'
+import './style.css'
 import { maquinasytecnologia } from './secciones';
 import { materialDeportivo } from './secciones';
 import { gastronomia } from './secciones';
 const main=document.querySelector('main');   
 const tecno=document.querySelector('#tecno');
-const sport=document.querySelector('#sport');
+  const sport=document.querySelector('#sport');
 const foods=document.querySelector('#foods');
 tecno.addEventListener("click", ()=>{
   for (let i=0; i<maquinasytecnologia.length; i++){
     const producto = maquinasytecnologia[i]; 
-const article=document.createElement('article')
+const article=document.createElement('article');
 article.innerHTML=`
-<h3>${producto.nombre}</h3>
-<h3>${producto.precio}</h3>
-<h3>${producto.disponible}</h3>
+<p>${producto.nombre}</p>
+<p>${producto.precio}</p>
+<p>${producto.disponible}</p>
 `
 main.appendChild(article);
-
+  }
+});
+sport.addEventListener("click", ()=>{
+  for (let i=0; i<materialDeportivo.length; i++){
+    const producto2=materialDeportivo[i];
+ const article2=document.createElement('article');
+ article2.innerHTML=`
+ <p>${producto2.nombre}</p>
+ <p>${producto2.precio}</p>
+ <p>${producto2.disponible}</p>
+ `
+main.appendChild(article2);
+  }
+})
+foods.addEventListener("click", ()=>{
+  for (let i=0; i<gastronomia.length; i++){
+    const producto3 = gastronomia[i];
+  const article3=document.createElement('article');
+  article3.innerHTML=`
+  <p>${producto3.nombre}</p>
+  <p>${producto3.precio}</p>
+  <p>${producto3.disponible}</p>
+  `
+  main.appendChild(article3);
   }
 })
--- secciones.js
+++ secciones.js
@@ -1,38 +1,39 @@
 export const maquinasytecnologia=[{
    nombre: "Bitcoin Myner",
-   precio: 69,
-   disponible: true 
+ imagen: "https://res-console.cloudinary.com/djf4aknul/thumbnails/v1/image/upload/v1710878296/TcOhcXVpbmFfcGFyYV9taW5hcl9iaXRjb2luX2g4dWp0aw==/grid_landscape",  
+   precio: 69 + "€",
+   disponible: "Disponible: " + true 
 },
 {nombre: "Tablet Android",
- precio: 20,
- disponible: true   
+ precio: 20 + "€",
+ disponible: "Didponible: " + true   
 },
 {nombre: "Portátil HP",
-precio: 100,
-disponible: true,    
+precio: 100 + "€",
+disponible: "Disponible: " + true    
 }
 ];
 export const materialDeportivo=[{
 nombre: "Balón de fútbol",
-precio: 10,
-disponible: true    
+precio: 10 + "€",
+disponible: "Disponible: " + true    
 },
 {nombre: "Balón de baloncesto",
-  precio: 7,
-  disponible: true  
+  precio: 7 + "€",
+  disponible: "Disponible: " + true   
 }
 ];
 export const gastronomia=[{
 nombre: "Bandeja de sushi 10 piezas",
-precio: 6,
-disponible: true
+precio: 6 + "€",
+disponible: "Disponible: " + true
 },
-    {nombre: "Hamburguesa de carne con cebolla y pepinillos",
-   precio: 5,
-   disponible: true
+    {nombre: "Hamburguesa de carne con queso, cebolla, tomane y pepinillos",
+   precio: 5 + "€",
+   disponible: "Disponible: " + true
 },
 {nombre: "Kevap de ternera con tomate, lechuga y cebolla",
-precio: 15,
-disponible: true
+precio: 15 + "€",
+disponible: "Disponible: " + true
 }
 ];
